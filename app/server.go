b56514b0231fa57e143ace71a33a169ababe5e55
baevm <baev2m@gmail.com>
2024-11-10 12:19:26 +0500

    added tcp listener

--- app/server.go
+++ app/server.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const ADDR = "0.0.0.0:6379"
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	log.Println("Starting TCP listener on:", ADDR)
+	listener, err := net.Listen("tcp", ADDR)
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	if err != nil {
+		log.Fatalln("failed to start TCP listener: ", err.Error())
+	}
+
+	_, err = listener.Accept()
+
+	if err != nil {
+		log.Fatalln("failed to accept connection: ", err.Error())
+	}
 }
